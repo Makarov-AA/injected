bf5636a6b56e319c0aeb38afa3992e8b268ec3fe
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2026-04-19 16:52:49 +0300

    Rename injected to injectlib (the name is busy on PyPI)

--- setup.py
+++ setup.py
@@ -1,20 +1,67 @@
-"""Install and build injected distributions"""
+"""Install and build injectlib distributions"""
 
+import os
+import shutil
+import subprocess
+import sys
 from setuptools import setup, find_packages
 
-setup(name='injected',
+
+x86_cmake_arch_name, x64_cmake_arch_name = 'Win32', 'x64'
+x86_package_arch_name, x64_package_arch_name = 'x86', 'x64'
+
+is_64bit = sys.maxsize > 2**32
+if is_64bit:
+    arch_names_map = { x64_cmake_arch_name: x64_package_arch_name }
+else:
+    arch_names_map = { x86_cmake_arch_name: x86_package_arch_name }
+
+build_dirname = 'build_'
+build_dll_dirs = ['./backends/dotnet/', './backends/hook/', './backends/qt/']
+package_dll_dirs = ['./src/injectdll/libs/dotnet/', './src/injectdll/libs/hook/', './src/injectdll/libs/qt/']
+cmake_dirs = build_dll_dirs
+
+# cmake build for DLLs
+for arch in arch_names_map:
+    for cmake_dir in cmake_dirs:
+        build_dir = cmake_dir + build_dirname + arch
+
+        os.makedirs(build_dir, exist_ok=True)
+
+        subprocess.check_call(['cmake', '-B ' + build_dir, '-S ' + cmake_dir, '-A ' + arch])
+        subprocess.check_call(['cmake', '--build', build_dir, '--config', 'Release'])
+
+# copy DLLs to the package
+for arch in arch_names_map:
+    for build_dll_dir, package_dll_dir in zip(build_dll_dirs, package_dll_dirs):
+        for root, _, files in os.walk(build_dll_dir + build_dirname + arch):
+            for file in files:
+                if file.endswith('.dll'):
+                    src_file_path = os.path.join(root, file)
+                    dest_file_path = os.path.join(package_dll_dir + arch_names_map.get(arch), file)
+                    os.makedirs(os.path.dirname(dest_file_path), exist_ok=True)
+                    shutil.copy(src_file_path, dest_file_path)
+
+
+# mark the package is not pure Python code
+class BinaryDistribution(Distribution):
+    def is_pure(self):
+        return False
+
+
+setup(name='injectlib',
       version='0.0.1',
-      description='A set of Python modules to inject DLls into applications for the Microsoft Windows',
-      keywords="windows gui .net inject testing test desktop dll wpf",
-      url="https://github.com/pywinauto/injected",
-      author='Mark Mc Mahon and Contributors',
-      author_email='pywinauto@yandex.ru',
+      description='A set of Python modules to inject DLLs into applications for the Microsoft Windows',
+      keywords="windows gui .net inject testing test desktop dll wpf qt",
+      url="https://github.com/pywinauto/injectlib",
+      author='Vasily Ryabov, Ilya Naumov, Boris Galochkin, Alexander Makarov',
+      author_email='vasily.v.ryabov@gmail.com',
       long_description="""
 It allows to inject DLls into applications for the Microsoft Windows.
 """,
       platforms=['win32'],
 
-      packages=find_packages(where="src"),
+      packages=["injectdll"],
       package_dir={"": "src"},
       include_package_data=True,
 
@@ -34,7 +81,6 @@ It allows to inject DLls into applications for the Microsoft Windows.
           'Programming Language :: Python :: Implementation :: CPython',
           'Topic :: Software Development :: Libraries :: Python Modules',
           'Topic :: Software Development :: Testing',
-          'Topic :: Software Development :: User Interfaces'
       ],
       install_requires=['pywin32'],
       python_requires='>=3.7',
